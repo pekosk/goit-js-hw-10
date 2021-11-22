1d72d0f16a8ec202607655455e774233ddf59f1d
pekosk <rkostyk64@gmail.com>
2021-11-22 17:44:55 +0200

    template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import countryCardTemplate from './country-card.hbs';
+import countryListTemplate from './country-list.hbs';
+import { fetchCountries } from './fetchCountries';
 
+const countryInput = document.querySelector('#search-box');
+const countryCardsWrapperEl = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 300;
+
+const renderCountryCard = countryData => {
+  if (countryData.length > 1) {
+    countryCardsWrapperEl.insertAdjacentHTML('beforeend', countryListTemplate());
+  }
+};
+
+const searchCountry = (event) => {
+  const country = countryInput.value;
+
+  fetchCountries(country)
+  .then(countryData => {
+    console.log(countryData);
+    renderCountryCard(countryData);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+};
+
+const searchCountryInput = debounce(searchCountry, DEBOUNCE_DELAY);
+countryInput.addEventListener('input', searchCountryInput);
